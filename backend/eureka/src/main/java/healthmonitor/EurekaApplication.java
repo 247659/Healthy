11937446ca23f62c10d9349cc5119b78fe21f03d
CountToFour <247806@edu.p.lodz.pl>
2026-06-12 00:29:39 +0200

    eureka server

--- backend/eureka/src/main/java/healthmonitor/EurekaApplication.java
+++ backend/eureka/src/main/java/healthmonitor/EurekaApplication.java
@@ -2,14 +2,14 @@ package healthmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class ApiGatewayApplication {
+@EnableEurekaServer
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
